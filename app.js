85e1a84f6b1dfdf6b45adc5aef9c349320e8d48c
faraamcode <abdulrasaqalagbede@gmail.com>
2021-02-16 04:55:31 +0000

    subject api created

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const studentRoute = require('./route/studentRoute');
 const staffRoute = require('./route/staffRoute');
 const classRoute = require('./route/classRoute');
+const subjectRoute = require('./route/subjectRoute');
 const app = express();
 app.use(express.json())
 app.use(studentRoute)
 app.use(staffRoute)
 app.use(classRoute)
+app.use(subjectRoute)
 app.listen(3000)
