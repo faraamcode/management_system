0ae795ea649beaec92203211a8cc83048cafd844
faraamcode <abdulrasaqalagbede@gmail.com>
2021-03-05 12:51:09 +0100

    attendance  route

---
+++ controller/attendanceCont.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const pool = require('../db/connect')
+const { fieldvalue } = require('../model/psycomotorClass')
+const Query = require('../model/psycomotorClass')
+const table = 'year1_attendance'
+
+// inserting new record on th table
+
+exports.insertNewAttendance = async (req, res, next) => {
+    const fieldvalue = [
+      
+      req.body.admission_no,
+      req.body.class_id,
+      req.body.term,
+      req.body.session,
+      req.body.time_school_open,
+      req.body.present_time,
+      req.body.absent_time,
+    
+     
+    ]
+    const field = [
+      'admission_no',
+      'class_id',
+      'term',
+      'session',
+      'class_teacher',
+      'time_school_open',
+      'present_time',
+      'absent_time'
+    ]
+  
+    const QueryInstance = new Query(table, field)
+    QueryInstance.turnArray()
+    try {
+      const result = await QueryInstance.postAll(fieldvalue)
+      if (result.rowCount === 1) {
+        res.send({
+          message: 'new attendance created added saved',
+        })
+      } else {
+        res.send({
+          message: 'error occured',
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      res.send({ error })
+    }
+  }
+  // deleting students by admission no
+  
+  exports.DeleteComment = async (req, res, next) => {
+    const fields = await Query.turnUpdateArrayWithAND(['admission_no', 'term', 'session'])
+    const fieldvalue = [req.body.admission_no, req.body.term, req.body.session]
+    const result = await Query.DeleteWithMultiple(table, fields, fieldvalue)
+    if (result === 1) {
+      res.send({
+        message: `${req.body.admission_no} psycomotor successfully deleted`,
+      })
+    }
+  }
+  // updating psycomotor by multiple clause
+  
+  exports.UpdateComment = async (req, res, next) => {
+    const updatefields = ['class_teacher']
+    const clausefields = ['admission_no', 'term', 'session']
+    const fieldvalue = [req.body.class_teacher, req.body.admission_no, req.body.term, req.body.session]
+    const result = await Query.UpdateWithMultiple(table, updatefields, clausefields, fieldvalue)
+    if (result === 1) {
+      res.send({
+        message: `${req.body.admission_no} psycomotor successfully updated`,
+      })
+    }else{
+      res.send({
+        "message" : "error occured"
+      })
+    }
+  }
+  //   fetching psycomotor using multiple 
+  
+  exports.fetchByMultiple = async(req, res, next)=>{
+      const fields = ['admission_no', 'term', 'session']
+      const fieldvalue = [req.body.admission_no, req.body.term, req.body.session]
+      const result = await Query.fetchByMultiple(table, fields, fieldvalue)
+     
+        res.send(result)
+  
+  }
--- route/commentsRoute.js
+++ route/commentsRoute.js
@@ -1,14 +1,14 @@
 const pool = require('../db/connect')
 const express = require('express')
-const commentsController = require('../controller/commentsCont')
+const attendanceController = require('../controller/attendanceCont')
 const router = express.Router()
 
-// inserting new comment into the table
-router.post('/comment', commentsController.insertNewComment)
-// getting a student comment using admission, term, session
-router.get('/comment/student', commentsController.fetchByMultiple)
+// inserting new attendance into the table
+router.post('/attendance', attendanceController.insertNewAttendance)
+// getting a student attendance using admission, term, session
+router.get('/attendance/student', attendanceController.fetchByMultiple)
 // updating a student comment using admission, term , session
-router.post('/comment/update', commentsController.UpdateComment)
+router.post('/attendance/update', attendanceController.UpdateAttendance)
 //  deleting a student comment using admission, term, session
-router.post('/comment/delete', commentsController.DeleteComment)
+router.post('/attendance/delete', attendanceController.DeleteAttendance)
 module.exports = router
