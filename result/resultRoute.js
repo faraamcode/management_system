b98a37d959a9fe03340cd827fc32b7683254a15d
faraamcode <abdulrasaqalagbede@gmail.com>
2021-08-03 10:02:59 +0100

    refactoring class

--- result/resultRoute.js
+++ result/resultRoute.js
@@ -1,12 +1,12 @@
-const pool = require("../db/connect");
-const express = require("express");
-const pdf = require("html-pdf");
-const jwt = require("jsonwebtoken");
-const router = express.Router();
-const resultController = require("./resultCont");
-const pdfTemplate = require("../documents/index.jsx");
+const pool = require('../db/connect')
+const express = require('express')
+const pdf = require('html-pdf')
+const jwt = require('jsonwebtoken')
+const router = express.Router()
+const resultController = require('./resultCont')
+const pdfTemplate = require('../documents/index.jsx')
 
-router.post("/result/check", resultController.checkIfAvailable);
+router.post('/result/check', resultController.checkIfAvailable)
 
 /*
 ==========
@@ -15,31 +15,31 @@ ROUTES
 */
 // inputing new result
 router.post(
-  "/result",
+  '/result',
   resultController.checkIfAvailable,
   resultController.insertNewResult
-);
+)
 // inputing new second ca result
-router.post("/result/secondca", resultController.insertSecondCa);
+router.post('/result/secondca', resultController.insertSecondCa)
 
 // inputing new exam score
-router.post("/result/exam", resultController.insertExam);
+router.post('/result/exam', resultController.insertExam)
 
 // getting term result for a studennt
 router.post(
-  "/result/term",
+  '/result/term',
   resultController.getStudentTermResult,
   resultController.downloadResult
-);
+)
 
 // getting term result for a whole class
-router.get("/result/term/class", resultController.getClassTermResult);
+router.get('/result/term/class', resultController.getClassTermResult)
 
 // gettiing a session result for a student
-router.get("/result/session/student", resultController.getStudentSessionResult);
+router.get('/result/session/student', resultController.getStudentSessionResult)
 
 // getting a session result for a class
-router.get("/result/session/class", resultController.getClassSessionResult);
+router.get('/result/session/class', resultController.getClassSessionResult)
 
 /*
 ==========
@@ -49,4 +49,4 @@ LOGICS
 
 // getting the heightest and the lowest score a particular subject
 
-module.exports = router;
+module.exports = router
